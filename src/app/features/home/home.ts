413f8dea2ab287e1b68dc2cb3c2a8d98db1d97bd
Marco Tello <marcotello5@outlook.com>
2026-03-10 17:35:12 -0600

    Deleting all the angular files

--- src/app/features/home/home.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.html',
-  styleUrl: './home.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class Home {}
--- src/app/layout/main/footer/footer.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  templateUrl: './footer.html',
-  styleUrl: './footer.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class Footer {}
